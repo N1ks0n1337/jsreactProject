2c2c29749f7b33030f4ef7fccc9869a703f5213d
Nick <sirvach666@gmail.com>
2022-01-02 16:10:10 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+//const result = confirm(`are u here?`);
+//console.log(result);
+
+//const answer = +prompt(`Вам есть 18?`, `18`);
+//console.log(typeof(answer));
+
+const answers = [];
+
+//answers[0] = prompt(`Как ваше имя?`, ``);
+//answers[1] = prompt(`Сколько вам лет?`, ``);
+//answers[2] = prompt(`Как ваша фамилия?`, ``);
+//console.log(answers[0,1,2]);
+
+console.log(typeof(answers));
